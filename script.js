b70fa29181db0b552cd8370c6859eec223a4498c
MattiaBaietta <mattia.baietta89@gmail.com>
2023-11-15 14:07:49 +0100

    pag results completata

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let checkpos=document.getElementsByTagName("main")
 let domandacorrente=1
+let creadivpagina3=document.createElement("div")
+creadivpagina3.classList="classedivpg3"
+
+
 const questions = [
     {
       category: "Science: Computers",
@@ -125,17 +129,62 @@ function PickRandomQuestion(ListaDomande){ //,Numerodomande,Difficoulty quando f
             
         }
         else{
+            clearTimeout(timeout)
+            
             //creare classe css per il risultato
             document.getElementById("main").innerHTML=""
-            let checkpos=document.getElementById("main")
-            
+            let creabottonefooter=document.createElement("input")
+            let footerpos=document.getElementsByTagName("footer")
+            creabottonefooter.type="button"
+            creabottonefooter.classList="bottonefooter"
+            creabottonefooter.value="RATE US"
+            let creadivresults=document.createElement("div")
+            creadivresults.id="firstText"
+            let creah1=document.createElement("h1")
+            let creap=document.createElement("p")
+            creah1.innerText="Results"
+            creap.innerText="The summary of your answers:"
+            checkpos[0].appendChild(creadivresults)
+            creadivresults.appendChild(creah1)
+            creadivresults.appendChild(creap)
+            checkpos[0].appendChild(creadivpagina3)
             let creacorrect=document.createElement("h4")
             let creawrong=document.createElement("h4")
-            creacorrect.innerText="Correct "+ (contatore/10)*100+"%" + contatore + "/10 questions"
+            let creadivcorrect=document.createElement("div")
+            let creadivwrong=document.createElement("div")
+            creadivcorrect.classList="elemento"
+            creadivwrong.classList="elemento"
+            creadivwrong.style="text-align:end"
+            
+
+            
+            creacorrect.innerText="Correct "
+            creaspan=document.createElement("span")
+            creaspan.style="font-weight:700;font-size:3.5em"
+            creaspan.innerText=(contatore/10)*100+"%"
+            creapquestion=document.createElement("p")
+            creapquestion.style="font-weight:200;margin-top: 0;"
+            creapquestion.innerText=contatore + "/10 questions"
+            creawrong.innerText="Wrong "
+            creaspanwrong=document.createElement("span")
+            creaspanwrong.style="font-weight:700;font-size:3.5em"
+            creaspanwrong.innerText=(10-contatore)/10*100+"%"
+            creapquestionwrong=document.createElement("p")
+            creapquestionwrong.style="font-weight:200;margin-top: 0;"
+            creapquestionwrong.innerText=(10-contatore) + "/10 questions"
+            creadivpagina3.appendChild(creadivcorrect)
+            creadivcorrect.appendChild(creacorrect)
+            creadivcorrect.appendChild(creaspan)
+            creadivcorrect.appendChild(creapquestion)
+            
+            
             CreaRisultato(100-(contatore*10))
-            creawrong.innerText="Wrong " + (10-contatore)/10*100+"%" + (10-contatore) + "/10 questions"
-            checkpos.appendChild(creacorrect)
-            creacorrect.appendChild(creawrong)
+            creadivpagina3.appendChild(creadivwrong)
+            creadivwrong.appendChild(creawrong)
+            creadivwrong.appendChild(creaspanwrong)
+            creadivwrong.appendChild(creapquestionwrong)
+            creabottonefooter.style=""
+            footerpos[0].appendChild(creabottonefooter)
         
         }
         
@@ -187,15 +236,18 @@ function CreaRisultato(percent){
 
  
   percent = Math.min(100, Math.max(0, percent));
-  var dashLength = (percent / 100) * 565.48;
-  checkpos[0].innerHTML=`
-  <svg id="circular-progress" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+  var dashLength = (percent / 100) * 1131;
+  let creadivsvg=document.createElement("div")
+  creadivsvg.id="progress-container"
+  creadivpagina3.appendChild(creadivsvg).innerHTML=`
+  <svg id="circular-progress" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
   
     
-    <circle  cx="100" cy="100" r="90" fill="none" stroke="#00ffff" transform="rotate(270 100 100)" stroke-width="9" />
+    <circle  cx="-20" cy="250" r="180" fill="none" stroke="#00ffff" transform="rotate(270 100 100)" stroke-width="57" />
+    <text  x="280" y="120" text-anchor="middle" alignment-baseline="middle" fill="white"><tspan x="250" dy="1.2em" style="font-weight: bold;font-size:1.1em">Congratulations!</tspan><tspan fill="#00ffff" x="250" dy="1.2em" style="font-weight: bold;font-size:1.1em" >You Passed the exam.</tspan><tspan x="250" dy="3.5em"> We'll send you the certificate</tspan><tspan x="250" dy="1.2em">in few minutes.</tspan><tspan x="270" dy="1.2em">Check you email (including</tspan><tspan x="270" dy="1.2em">promotions/spam folder)<tspan></text>
 
     
-    <circle cx="100" cy="100" r="90" fill="none" stroke="#d20094" transform="rotate(270 100 100)" stroke-width="10"  stroke-dasharray="`+ dashLength +`, 565.48" />
+    <circle cx="-20" cy="250" r="180" fill="none" stroke="#d20094" transform="rotate(270 100 100)" stroke-width="60"  stroke-dasharray="`+ dashLength +`, 1131" />
   </svg>`
 
 }
@@ -240,4 +292,3 @@ function PrimaPagina(){
     
 }
 PrimaPagina()
-
