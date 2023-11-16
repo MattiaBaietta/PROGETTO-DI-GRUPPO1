0922efe64431eddf483eab11763a56035cf6192c
MattiaBaietta <mattia.baietta89@gmail.com>
2023-11-16 13:39:40 +0100

    fine pagina feedback

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let checkpos = document.getElementsByTagName("main")
 let domandacorrente = 1
 let creadivpagina3 = document.createElement("div")
 creadivpagina3.classList = "classedivpg3"
+let numerodomande=10
 
 
 const questions = [
@@ -109,7 +110,7 @@ let timeout
 function PickRandomQuestion(ListaDomande) { //,Numerodomande,Difficoulty quando farà gli extra
   let i2 = 0
   checkpos[0].style = "margin-top:150px"
-  console.log(contatore)//contatore funziona 
+  
 
   i2 = Math.floor(Math.random() * ListaDomande.length)
   if (document.getElementById("progress-container2")) {
@@ -144,8 +145,9 @@ function PickRandomQuestion(ListaDomande) { //,Numerodomande,Difficoulty quando 
     creabottonefooter.type="button"
     creabottonefooter.classList="bottonefooter"
     creabottonefooter.value="RATE US"
+    
     let creadivresults=document.createElement("div")
-    creadivresults.id="firstText"
+    creadivresults.classList="firstText"
     let creah1=document.createElement("h1")
     let creap=document.createElement("p")
     creah1.innerText="Results"
@@ -191,6 +193,10 @@ function PickRandomQuestion(ListaDomande) { //,Numerodomande,Difficoulty quando 
     creadivwrong.appendChild(creapquestionwrong)
     creabottonefooter.style=""
     footerpos[0].appendChild(creabottonefooter)
+    creabottonefooter.addEventListener("click",function () {
+
+      addFeedback()
+    })
 
 }
 
@@ -208,7 +214,7 @@ let percentage = 100;
 
 function creaTimer() {
 
-
+  
   let elapsedSeconds = 20;
   let creadivtimer = document.createElement("div")
   creadivtimer.id = "progress-container2"
@@ -243,11 +249,11 @@ function creaTimer() {
   const durationInSeconds = 20;
   const initialDashOffset = parseFloat(getComputedStyle(creacircle2).strokeDasharray);
   const dashOffsetPerSecond = initialDashOffset / durationInSeconds;
-
+  
   function startTimer() {
     let dashOffset = Math.max(0, initialDashOffset - dashOffsetPerSecond * elapsedSeconds);
     creacircle2.style.strokeDashoffset = dashOffset;
-    console.log(creacircle2.style.strokeDashoffset)
+    
     circletext.setAttribute("y", "31")
     circletext.innerHTML = `<tspan style="font-size:0.8em" x="110" dy="1em" >SECONDS</tspan>
       <tspan style="font-size:3em" x="110" dy="1em">${elapsedSeconds}</tspan>
@@ -255,7 +261,7 @@ function creaTimer() {
     const timerInterval = setInterval(() => {
       elapsedSeconds--;
       dashOffset = -1 * Math.max(0, initialDashOffset - dashOffsetPerSecond * elapsedSeconds);
-      console.log(dashOffset)
+      
       creacircle2.style.strokeDashoffset = dashOffset;
 
 
@@ -263,47 +269,20 @@ function creaTimer() {
       circletext.innerHTML = `<tspan style="font-size:0.8em" x="110" dy="1em" >SECONDS</tspan>
       <tspan style="font-size:3em" x="110" dy="1em">${elapsedSeconds}</tspan>
       <tspan style="font-size:0.8em" x="110"  dy="1.5em">REMAINING</tspan>`;
-      if (elapsedSeconds <= 0) {
+      if (elapsedSeconds < 0) {
         clearInterval(timerInterval);
+        
       }
     }, 1000);
 
   }
-
+  
   startTimer()
-
-
-
-
-
-
-
-
-
-
-
-  /*percentage = Math.min(100, Math.max(0, percentage));
-  let dashLength = (percentage / 100) * 377.09;
-  console.log(dashLength)
-
-  let creadivtimer=document.createElement("div")
-  creadivtimer.id="progress-container2"
-  document.getElementsByTagName("header")[0].appendChild(creadivtimer)
-  creadivtimer.innerHTML=`
-  <svg height="100%" width="100%" transform: rotate(270deg)>
-  <!-- Cerchio esterno (contorno) -->
-  <circle id="outer-circle" cx="50%" cy="50%" r="55" stroke-dasharray="></circle>
-
-  <!-- Cerchio di riempimento -->
-  <circle id="progress-circle" cx="50%" cy="50%" r="55" stroke-dasharray="`+ dashLength +`, 377.09"></circle>
-  <text id="progress-text" x="50%" y="50%">0%</text>
-</svg>`*/
-
 
 
 }
 function CreaBottone(domanda) {
-
+  
   let checkpos = document.getElementById("main")
   let creaform = document.createElement("form")
   let creadiv = document.createElement("div")
@@ -409,7 +388,7 @@ function PrimaPagina() {
   checkpos[0].appendChild(createbutton)
   let trovacheckbox = document.getElementById("checkbox1")
 
-  console.log(trovacheckbox.checked)
+  
   createbutton.addEventListener("click", function () {
 
     if (trovacheckbox.checked) {
@@ -461,6 +440,94 @@ function aggiornaPosizioneTextbox(event) {
   textbox.style.top = (event.clientY + window.scrollY + 10) + "px"; // Aggiunge 10 pixel per separare il cursore dalla textbox
 }
 
+
+
+function addFeedback(){
+  checkpos[0].innerHTML=""
+  checkpos[0].style="text-align: center;"
+  checkpos[0].classList.add="firstText"
+  document.getElementsByClassName("bottonefooter")[0].remove()
+  let creah2=document.createElement("h1")
+  //creah2.classList="h1feedback"
+  let creap=document.createElement("p")
+  creah2.innerText="Tell us how it's going"
+  creap.innerText="From 0 to 10, how likely are you to reccommend EPICODE to a friend or a colleague?"
+  checkpos[0].appendChild(creah2)
+  checkpos[0].appendChild(creap)
+  for (let i=1;i<=10;i++)
+  {
+      let nuovoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      nuovoSvg.classList="classestelline"
+      nuovoSvg.setAttribute("width","47")
+      nuovoSvg.setAttribute("height","46")
+      nuovoSvg.setAttribute("fill","none")
+      nuovoSvg.setAttribute("viewBox", "0 0 47 46");
+      nuovoSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+      nuovoSvg.setAttribute("data-rating", i);
+      
+      
+        
+      
+      let nuovoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      nuovoPath.setAttribute("d", "M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z");
+      nuovoPath.setAttribute("fill","#0B113B")
+      nuovoPath.setAttribute("stroke","#00FFFF")
+      nuovoPath.setAttribute("stroke-width","2")
+      nuovoSvg.appendChild(nuovoPath);
+      checkpos[0].appendChild(nuovoSvg);
+      
+
+      nuovoSvg.addEventListener("mouseover", ()=>{mousedentro(i)})
+      nuovoSvg.addEventListener("mouseout",()=>{ mousefuori(i)}) 
+      nuovoSvg.addEventListener("click", ()=>{funzioneclick(i)})
+
+
+  }
+  let creap2=document.createElement("p")
+  creap2.innerText="Leave us an open feedback about your experience so far "
+  let creainput=document.createElement("input")
+  creainput.setAttribute("type","text")
+  creainput.id="write"
+  creainput.placeholder="Write your comment here"
+  
+  let creariga=document.createElement("p")
+  creariga.id="lilla"
+  checkpos[0].appendChild(creap2)
+  checkpos[0].appendChild(creainput)
+  checkpos[0].appendChild(creariga)
+
+
+
+  function mousedentro(i){
+    for(let i2=i;i2>0;i2--){
+      document.querySelector(`svg[data-rating="`+i2+`"]`).children[0].classList="classeblue";
+
+    }
+  }
+  function mousefuori(i){
+    for(let i2=i;i2>0;i2--){
+      document.querySelector(`svg[data-rating="`+i2+`"]`).children[0].classList="classenera"
+    }
+  }
+  function funzioneclick(i) {      
+    for(let i2=10;i2>0;i2--){
+      
+      document.querySelector(`svg[data-rating="`+i2+`"]`).removeEventListener("mouseout",()=>{mousedentro(i2)})
+      document.querySelector(`svg[data-rating="`+i2+`"]`).removeEventListener("mouseover",()=>{ mousefuori(i2)})
+      
+    }
+    for(i;i>0;i--)
+    {
+      document.querySelector(`svg[data-rating="`+i+`"]`).children[0].style.fill="#00FFFF";
+    }
+    
+    //nuovoSvg.removeEventListener("click",funzioneclick(i))
+  }
+  
+  
+
+
+}
 
 
 
